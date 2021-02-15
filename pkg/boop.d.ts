2ba0d0e2edd6574304409cd8dce087dbd10d4123
aspunds <jonathan@asplund.net>
2021-02-15 01:14:34 +0100

    initial commit

---
+++ pkg/boop.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} name
+*/
+export function greet(name: string): void;
+/**
+* @param {string} num
+* @returns {number}
+*/
+export function square(num: string): number;
---
+++ pkg/boop.js
@@ -0,0 +1,2 @@
+import * as wasm from "./boop_bg.wasm";
+export * from "./boop_bg.js";
